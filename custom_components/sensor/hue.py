838f93b809ee527e7db836f3429de85812d18d49
Vladimir Eremin <me@yottatsa.name>
2019-01-03 18:20:05 +0000

    bump to 1.0.2

--- custom_components/sensor/hue.py
+++ custom_components/sensor/hue.py
@@ -17,7 +17,7 @@ from homeassistant.helpers.event import async_track_time_interval
 
 DEPENDENCIES = ["hue"]
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 _LOGGER = logging.getLogger(__name__)
 
