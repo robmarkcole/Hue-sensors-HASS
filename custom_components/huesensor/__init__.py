eabfe7c7b35609d435443cc96a8b10df9581ee52
Robin Cole <robmarkcole@gmail.com>
2020-02-14 04:47:31 +0000

    Update __init__.py

--- custom_components/huesensor/__init__.py
+++ custom_components/huesensor/__init__.py
@@ -1,10 +1,6 @@
 """The huesensors component."""
 import asyncio
-import logging
-
 import async_timeout
-
-_LOGGER = logging.getLogger(__name__)
 
 
 def get_bridges(hass):
@@ -25,6 +21,5 @@ async def update_api(api):
         with async_timeout.timeout(10):
             await api.update()
     except (asyncio.TimeoutError, aiohue.AiohueException) as err:
-        _LOGGER.debug("Failed to fetch sensors: %s", err)
         return False
     return True
