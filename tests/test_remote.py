24e8bf89c9ffb09c0377513396a24862a8e8794c
Eugenio Panadero <eugenio.panadero@gmail.com>
2020-03-09 20:40:28 +0100

    Adapt imports in tests and add a new one for z3 switch
    (JSON extracted from #188#issuecomment-580800865)

--- tests/test_remote.py
+++ tests/test_remote.py
@@ -1,7 +1,9 @@
-"""
-Tests for binary_sensor.py
-"""
-import custom_components.huesensor.remote as remote
+"""Tests for remote.py."""
+from custom_components.huesensor.hue_api_response import (
+    parse_rwl,
+    parse_zgp,
+    parse_z3_rotary,
+)
 
 MOCK_ZGP = {
     "state": {"buttonevent": 17, "lastupdated": "2019-06-22T14:43:50"},
@@ -14,9 +16,8 @@ MOCK_ZGP = {
     "productname": "Hue tap switch",
     "diversityid": "d8cde5d5-0eef-4b95-b0f0-71ddd2952af4",
     "uniqueid": "00:00:00:00:00:44:23:08-f2",
-    "capabilities": {"certified": True, "primary": True, "inputs": [],},
+    "capabilities": {"certified": True, "primary": True, "inputs": []},
 }
-
 MOCK_RWL = {
     "state": {"buttonevent": 4002, "lastupdated": "2019-12-28T21:58:02"},
     "swupdate": {"state": "noupdates", "lastinstall": "2019-10-13T13:16:15"},
@@ -31,6 +32,37 @@ MOCK_RWL = {
     "uniqueid": "00:17:88:01:10:3e:3a:dc-02-fc00",
     "capabilities": {"certified": True, "primary": True, "inputs": []},
 }
+MOCK_Z3_ROTARY = {
+    "state": {
+        "rotaryevent": 2,
+        "expectedrotation": 208,
+        "expectedeventduration": 400,
+        "lastupdated": "2020-01-31T15:56:19",
+    },
+    "swupdate": {"state": "noupdates", "lastinstall": "2019-11-26T03:35:21"},
+    "config": {"on": True, "battery": 100, "reachable": True, "pending": []},
+    "name": "Lutron Aurora 1",
+    "type": "ZLLRelativeRotary",
+    "modelid": "Z3-1BRL",
+    "manufacturername": "Lutron",
+    "productname": "Lutron Aurora",
+    "diversityid": "2c3a75ff-55c4-4e4d-8c44-82d330b8eb9b",
+    "swversion": "3.4",
+    "uniqueid": "ff:ff:00:0f:e7:fd:ba:b7-01-fc00-0014",
+    "capabilities": {
+        "certified": True,
+        "primary": True,
+        "inputs": [
+            {
+                "repeatintervals": [400],
+                "events": [
+                    {"rotaryevent": 1, "eventtype": "start"},
+                    {"rotaryevent": 2, "eventtype": "repeat"},
+                ],
+            }
+        ],
+    },
+}
 
 PARSED_ZGP = {
     "last_button_event": "3_click",
@@ -39,7 +71,6 @@ PARSED_ZGP = {
     "name": "Hue Tap",
     "state": "3_click",
 }
-
 PARSED_RWL = {
     "battery": 100,
     "last_button_event": "4_click_up",
@@ -50,8 +81,20 @@ PARSED_RWL = {
     "reachable": True,
     "state": "4_click_up",
 }
+PARSED_Z3_ROTARY = {
+    "model": "Z3-",
+    "name": "Lutron Aurora 1",
+    "dial_state": "end",
+    "dial_position": 208,
+    "software_update": "noupdates",
+    "battery": 100,
+    "on": True,
+    "reachable": True,
+    "last_updated": ["2020-01-31", "15:56:19"],
+}
 
 
 def test_parse_zgp():
-    assert remote.parse_zgp(MOCK_ZGP) == PARSED_ZGP
-    assert remote.parse_rwl(MOCK_RWL) == PARSED_RWL
+    assert parse_zgp(MOCK_ZGP) == PARSED_ZGP
+    assert parse_rwl(MOCK_RWL) == PARSED_RWL
+    assert parse_z3_rotary(MOCK_Z3_ROTARY) == PARSED_Z3_ROTARY
