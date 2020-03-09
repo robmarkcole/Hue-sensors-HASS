0fa59143e174008f60bef8d472186d3af1d72bf4
Eugenio Panadero <eugenio.panadero@gmail.com>
2020-03-09 20:29:54 +0100

    Refactor remote and binary_sensor
    - Add a new HueSensorData handler in data_manager.py,
    to use the same time tracker and make the update for all entities at once.
    - Optionally define the scan_interval for each platform
      (but it will just use the minor one for both platforms, and default is 0.5)
    - Scheduling starts in added_to_hass, so no updates are done before that
      (we could implement RestoreEntity to load the last state on that point)
    - Ready to use better registry, so entities could appear under integrations with
      small changes.
    
    Also a `HueSensorBaseDevice` has been refactored, as most of the entity methods
    are the same for both platforms.

--- custom_components/huesensor/binary_sensor.py
+++ custom_components/huesensor/binary_sensor.py
@@ -1,221 +1,46 @@
-"""
-Binary sensor for Hue motion sensors.
-"""
-import asyncio
+"""Binary sensor for Hue motion sensors."""
 import logging
-import threading
-from datetime import timedelta
 
 from homeassistant.components.binary_sensor import BinarySensorDevice
-from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import CONF_SCAN_INTERVAL, STATE_OFF, STATE_ON
-from homeassistant.helpers.event import async_track_time_interval
+from homeassistant.components.sensor import PLATFORM_SCHEMA  # noqa: F401
+from homeassistant.const import CONF_SCAN_INTERVAL, STATE_ON
 
-from . import get_bridges
-
+from . import DOMAIN
+from .data_manager import (
+    DEFAULT_SCAN_INTERVAL,
+    HueSensorBaseDevice,
+    HueSensorData,
+)
+from .hue_api_response import BINARY_SENSOR_MODELS
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_SCAN_INTERVAL = timedelta(seconds=0.5)
 TYPE_GEOFENCE = "Geofence"
 DEVICE_CLASSES = {"SML": "motion"}
-ATTRS = {
-    "SML": [
-        "light_level",
-        "battery",
-        "last_updated",
-        "lx",
-        "dark",
-        "daylight",
-        "temperature",
-        "on",
-        "reachable",
-        "sensitivity",
-        "threshold_dark",
-        "threshold_offset",
-    ]
-}
 
 
-def parse_hue_api_response(sensors):
-    """Take in the Hue API json response."""
-    data_dict = {}  # The list of sensors, referenced by their hue_id.
-
-    # Loop over all keys (1,2 etc) to identify sensors and get data.
-    for sensor in sensors:
-        modelid = sensor["modelid"][0:3]
-        if modelid == "SML":
-            _key = modelid + "_" + sensor["uniqueid"][:-5]
-            if _key not in data_dict:
-                data_dict[_key] = parse_sml(sensor)
-            else:
-                data_dict[_key].update(parse_sml(sensor))
-
-    return data_dict
-
-
-def parse_sml(response):
-    """Parse the json for a SML Hue motion sensor and return the data."""
-    if response["type"] == "ZLLLightLevel":
-        lightlevel = response["state"]["lightlevel"]
-        tholddark = response["config"]["tholddark"]
-        tholdoffset = response["config"]["tholdoffset"]
-        if lightlevel is not None:
-            lx = round(float(10 ** ((lightlevel - 1) / 10000)), 2)
-            dark = response["state"]["dark"]
-            daylight = response["state"]["daylight"]
-            data = {
-                "light_level": lightlevel,
-                "lx": lx,
-                "dark": dark,
-                "daylight": daylight,
-                "threshold_dark": tholddark,
-                "threshold_offset": tholdoffset,
-            }
-        else:
-            data = {
-                "light_level": "No light level data",
-                "lx": None,
-                "dark": None,
-                "daylight": None,
-                "threshold_dark": None,
-                "threshold_offset": None,
-            }
-
-    elif response["type"] == "ZLLTemperature":
-        if response["state"]["temperature"] is not None:
-            data = {"temperature": response["state"]["temperature"] / 100.0}
-        else:
-            data = {"temperature": "No temperature data"}
-
-    elif response["type"] == "ZLLPresence":
-        name_raw = response["name"]
-        arr = name_raw.split()
-        arr.insert(-1, "motion")
-        name = " ".join(arr)
-        hue_state = response["state"]["presence"]
-        if hue_state is True:
-            state = STATE_ON
-        else:
-            state = STATE_OFF
-
-        data = {
-            "model": "SML",
-            "name": name,
-            "state": state,
-            "battery": response["config"]["battery"],
-            "on": response["config"]["on"],
-            "reachable": response["config"]["reachable"],
-            "sensitivity": response["config"]["sensitivity"],
-            "last_updated": response["state"]["lastupdated"].split("T"),
-        }
-    return data
-
-
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+async def async_setup_platform(
+    hass, config, async_add_entities, discovery_info=None
+):
     """Initialise Hue Bridge connection."""
-    data = HueSensorData(hass, async_add_entities)
-    await data.async_update_info()
-    async_track_time_interval(
-        hass,
-        data.async_update_info,
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = HueSensorData(hass)
+
+    await hass.data[DOMAIN].async_add_platform_entities(
+        HueBinarySensor,
+        BINARY_SENSOR_MODELS,
+        async_add_entities,
         config.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL),
     )
 
 
-class HueSensorData(object):
-    """Get the latest sensor data."""
-
-    def __init__(self, hass, async_add_entities):
-        """Initialize the data object."""
-        self.hass = hass
-        self.lock = threading.Lock()
-        self.data = {}
-        self.sensors = {}
-        self.async_add_entities = async_add_entities
-
-    async def update_bridge(self, bridge):
-        await bridge.sensor_manager.coordinator.async_request_refresh()
-
-        data = parse_hue_api_response(
-            sensor.raw
-            for sensor in bridge.api.sensors.values()
-            if sensor.type != TYPE_GEOFENCE
-        )
-
-        new_sensors = data.keys() - self.data.keys()
-        updated_sensors = []
-        for key, new in data.items():
-            new["changed"] = True
-            old = self.data.get(key)
-            if not old or old == new:
-                continue
-            updated_sensors.append(key)
-            if (
-                old["last_updated"] == new["last_updated"]
-                and old["state"] == new["state"]
-            ):
-                new["changed"] = False
-        self.data.update(data)
-
-        new_entities = {
-            entity_id: HueSensor(entity_id, self) for entity_id in new_sensors
-        }
-        if new_entities:
-            _LOGGER.debug("Created %s", ", ".join(new_entities.keys()))
-            self.sensors.update(new_entities)
-            self.async_add_entities(new_entities.values(), True)
-        for entity_id in updated_sensors:
-            self.sensors[entity_id].async_write_ha_state()
-
-    async def async_update_info(self, now=None):
-        """Get the bridge info."""
-        locked = self.lock.acquire(False)
-        if not locked:
-            return
-        try:
-            bridges = get_bridges(self.hass)
-            if not bridges:
-                if now:
-                    # periodic task
-                    await asyncio.sleep(5)
-                return
-            await asyncio.wait(
-                [self.update_bridge(bridge) for bridge in bridges], loop=self.hass.loop
-            )
-        finally:
-            self.lock.release()
-
-
-class HueSensor(BinarySensorDevice):
-    """Class to hold Hue Sensor basic info."""
-
-    def __init__(self, hue_id, data):
-        """Initialize the sensor object."""
-        self._hue_id = hue_id
-        self._data = data.data  # data is in .data
-
-    @property
-    def should_poll(self):
-        """No polling needed."""
-        return False
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        data = self._data.get(self._hue_id)
-        if data:
-            return data["name"]
-
-    @property
-    def unique_id(self):
-        """Return the ID of this Hue sensor."""
-        return self._hue_id
+class HueBinarySensor(HueSensorBaseDevice, BinarySensorDevice):
+    """Class to hold Hue Binary Sensor basic info."""
 
     @property
     def is_on(self):
         """Return the state of the sensor."""
-        data = self._data.get(self._hue_id)
+        data = self.sensor_data
         if data and data["model"] == "SML" and data["changed"]:
             return data["state"] == STATE_ON
         return False
@@ -223,15 +48,4 @@ class HueSensor(BinarySensorDevice):
     @property
     def device_class(self):
         """Return the class of this device, from component DEVICE_CLASSES."""
-        data = self._data.get(self._hue_id)
-        if data:
-            device_class = DEVICE_CLASSES.get(data["model"])
-            if device_class:
-                return device_class
-
-    @property
-    def device_state_attributes(self):
-        """Attributes."""
-        data = self._data.get(self._hue_id)
-        if data:
-            return {key: data.get(key) for key in ATTRS.get(data["model"], [])}
+        return DEVICE_CLASSES.get(self.sensor_data["model"])
--- custom_components/huesensor/remote.py
+++ custom_components/huesensor/remote.py
@@ -1,25 +1,20 @@
-"""
-Hue remotes.
-"""
-import asyncio
+"""Hue remotes."""
 import logging
-import threading
-from datetime import timedelta
 
 from homeassistant.const import CONF_SCAN_INTERVAL
-from homeassistant.components.hue import HueBridge
-from homeassistant.components.remote import PLATFORM_SCHEMA, RemoteDevice
-from homeassistant.helpers.entity import Entity, ToggleEntity
-from homeassistant.helpers.event import async_track_time_interval
+from homeassistant.components.remote import PLATFORM_SCHEMA, RemoteDevice  # noqa: F401
 
-from . import get_bridges
-
+from . import DOMAIN
+from .data_manager import (
+    DEFAULT_SCAN_INTERVAL,
+    HueSensorBaseDevice,
+    HueSensorData,
+)
+from .hue_api_response import REMOTE_MODELS
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_SCAN_INTERVAL = timedelta(seconds=0.5)
-
-ICONS = {
+REMOTE_ICONS = {
     "RWL": "mdi:remote",
     "ROM": "mdi:remote",
     "ZGP": "mdi:remote",
@@ -27,324 +22,40 @@ ICONS = {
     "Z3-": "mdi:light-switch",
 }
 
-ATTRS = {
-    "RWL": ["last_updated", "last_button_event", "battery", "on", "reachable"],
-    "ROM": ["last_updated", "last_button_event", "battery", "on", "reachable"],
-    "ZGP": ["last_updated", "last_button_event"],
-    "FOH": ["last_updated", "last_button_event"],
-    "Z3-": [
-        "last_updated",
-        "last_button_event",
-        "battery",
-        "on",
-        "reachable",
-        "dial_state",
-        "dial_position",
-        "software_update",
-    ],
-}
 
-
-def parse_hue_api_response(sensors):
-    """Take in the Hue API json response."""
-    data_dict = {}  # The list of sensors, referenced by their hue_id.
-
-    # Loop over all keys (1,2 etc) to identify sensors and get data.
-    for sensor in sensors:
-        modelid = sensor["modelid"][0:3]
-        if modelid in ["RWL", "ROM"]:
-            _key = modelid + "_" + sensor["uniqueid"][:-5]
-            if modelid == "RWL" or modelid == "ROM":
-                data_dict[_key] = parse_rwl(sensor)
-
-        elif modelid in ["FOH", "ZGP"]:  ############# New Model ID
-            _key = modelid + "_" + sensor["uniqueid"][-14:-3]  ###needed for uniqueness
-            if modelid == "FOH":
-                data_dict[_key] = parse_foh(sensor)
-            elif modelid == "ZGP":
-                data_dict[_key] = parse_zgp(sensor)
-
-        elif (
-            modelid == "Z3-"
-        ):  # Newest Model ID / Lutron Aurora / Hue Bridge treats it as two sensors, I wanted them combined
-            if sensor["type"] == "ZLLRelativeRotary":  # Rotary Dial
-                _key = (
-                    modelid + "_" + sensor["uniqueid"][:-5]
-                )  # Rotary key is substring of button
-                key_value = parse_z3_rotary(sensor)
-            else:
-                _key = modelid + "_" + sensor["uniqueid"]
-                key_value = parse_z3_switch(sensor)
-
-            # Combine parsed data
-            if _key in data_dict:
-                data_dict[_key].update(key_value)
-            else:
-                data_dict[_key] = key_value
-
-    return data_dict
-
-
-def parse_zgp(response):
-    """Parse the json response for a ZGPSWITCH Hue Tap."""
-    TAP_BUTTONS = {34: "1_click", 16: "2_click", 17: "3_click", 18: "4_click"}
-    press = response["state"]["buttonevent"]
-    if press is None or press not in TAP_BUTTONS:
-        button = "No data"
-    else:
-        button = TAP_BUTTONS[press]
-
-    data = {
-        "model": "ZGP",
-        "name": response["name"],
-        "state": button,
-        "last_button_event": button,
-        "last_updated": response["state"]["lastupdated"].split("T"),
-    }
-    return data
-
-
-def parse_rwl(response):
-    """Parse the json response for a RWL Hue remote."""
-    responsecodes = {"0": "_click", "1": "_hold", "2": "_click_up", "3": "_hold_up"}
-
-    button = None
-    if response["state"]["buttonevent"]:
-        press = str(response["state"]["buttonevent"])
-        button = str(press)[0] + responsecodes[press[-1]]
-
-    data = {
-        "model": "RWL",
-        "name": response["name"],
-        "state": button,
-        "battery": response["config"]["battery"],
-        "on": response["config"]["on"],
-        "reachable": response["config"]["reachable"],
-        "last_button_event": button,
-        "last_updated": response["state"]["lastupdated"].split("T"),
-    }
-    return data
-
-
-def parse_foh(response):
-    """Parse the JSON response for a FOHSWITCH (type still = ZGPSwitch)"""
-    FOH_BUTTONS = {
-        16: "left_upper_press",
-        20: "left_upper_release",
-        17: "left_lower_press",
-        21: "left_lower_release",
-        18: "right_lower_press",
-        22: "right_lower_release",
-        19: "right_upper_press",
-        23: "right_upper_release",
-        100: "double_upper_press",
-        101: "double_upper_release",
-        98: "double_lower_press",
-        99: "double_lower_release",
-    }
-
-    press = response["state"]["buttonevent"]
-    if press is None or press not in FOH_BUTTONS:
-        button = "No data"
-    else:
-        button = FOH_BUTTONS[press]
-
-    data = {
-        "model": "FOH",
-        "name": response["name"],
-        "state": button,
-        "last_button_event": button,
-        "last_updated": response["state"]["lastupdated"].split("T"),
-    }
-    return data
-
-
-def parse_z3_rotary(response):
-    """Parse the json response for a Lutron Aurora Rotary Event."""
-
-    Z3_DIAL = {1: "begin", 2: "end"}
-
-    turn = response["state"]["rotaryevent"]
-    dial_position = response["state"]["expectedrotation"]
-    if turn is None or turn not in Z3_DIAL:
-        dial = "No data"
-    else:
-        dial = Z3_DIAL[turn]
-
-    data = {
-        "model": "Z3-",
-        "name": response["name"],
-        "dial_state": dial,
-        "dial_position": dial_position,
-        "software_update": response["swupdate"]["state"],
-        "battery": response["config"]["battery"],
-        "on": response["config"]["on"],
-        "reachable": response["config"]["reachable"],
-        "last_updated": response["state"]["lastupdated"].split("T"),
-    }
-    return data
-
-
-def parse_z3_switch(response):
-    """Parse the json response for a Lutron Aurora."""
-
-    Z3_BUTTON = {
-        1000: "initial_press",
-        1001: "repeat",
-        1002: "short_release",
-        1003: "long_release",
-    }
-
-    press = response["state"]["buttonevent"]
-    if press is None or press not in Z3_BUTTON:
-        button = "No data"
-    else:
-        button = Z3_BUTTON[press]
-
-    data = {"last_button_event": button, "state": button}
-    return data
-
-
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+async def async_setup_platform(
+    hass, config, async_add_entities, discovery_info=None
+):
     """Initialise Hue Bridge connection."""
-    _LOGGER.warning(f"config: {config}")
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = HueSensorData(hass)
 
-    data = HueRemoteData(hass, async_add_entities)
-    await data.async_update_info()
-    async_track_time_interval(
-        hass,
-        data.async_update_info,
+    await hass.data[DOMAIN].async_add_platform_entities(
+        HueRemote,
+        REMOTE_MODELS,
+        async_add_entities,
         config.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL),
     )
 
 
-class HueRemoteData(object):
-    """Get the latest sensor data."""
-
-    def __init__(self, hass, async_add_entities):
-        """Initialize the data object."""
-        self.hass = hass
-        self.lock = threading.Lock()
-        self.data = {}
-        self.sensors = {}
-        self.async_add_entities = async_add_entities
-
-    async def update_bridge(self, bridge: HueBridge):
-        await bridge.sensor_manager.coordinator.async_request_refresh()
-
-        data = parse_hue_api_response(
-            sensor.raw for sensor in bridge.api.sensors.values()
-        )
-
-        new_sensors = data.keys() - self.data.keys()
-        updated_sensors = []
-        for key, new in data.items():
-            old = self.data.get(key)
-            if not old or old == new:
-                continue
-            if (
-                old["last_updated"] == new["last_updated"]
-                and old["state"] == new["state"]
-            ):
-                continue
-            updated_sensors.append(key)
-        self.data.update(data)
-
-        new_entities = {
-            entity_id: HueRemote(entity_id, self) for entity_id in new_sensors
-        }
-        if new_entities:
-            _LOGGER.warning("Created %s", ", ".join(new_entities.keys()))
-            self.sensors.update(new_entities)
-            self.async_add_entities(new_entities.values(), True)
-        for entity_id in updated_sensors:
-            self.sensors[entity_id].async_write_ha_state()
-
-    async def async_update_info(self, now=None):
-        """Get the bridge info."""
-        locked = self.lock.acquire(False)
-        if not locked:
-            return
-        try:
-            bridges = get_bridges(self.hass)
-            if not bridges:
-                if now:
-                    # periodic task
-                    await asyncio.sleep(5)
-                return
-            await asyncio.wait(
-                [self.update_bridge(bridge) for bridge in bridges], loop=self.hass.loop
-            )
-        finally:
-            self.lock.release()
-
-
-class HueRemote(RemoteDevice):
+class HueRemote(HueSensorBaseDevice, RemoteDevice):
     """Class to hold Hue Remote basic info."""
-
-    def __init__(self, hue_id, data):
-        """Initialize the remote object."""
-        self._hue_id = hue_id
-        self._data = data.data  # data is in .data
-
-    # async def async_added_to_hass(self):
-    #     """When entity is added to hass."""
-    #     self.bridge.sensor_manager.coordinator.async_add_listener(
-    #         self.async_write_ha_state
-    #     )
-    #
-    # async def async_will_remove_from_hass(self):
-    #     """When entity will be removed from hass."""
-    #     self.bridge.sensor_manager.coordinator.async_remove_listener(
-    #         self.async_write_ha_state
-    #     )
-    #
-    # async def async_update(self):
-    #     """Update the entity.
-    #
-    #     Only used by the generic entity update service.
-    #     """
-    #     await self.bridge.sensor_manager.coordinator.async_request_refresh()
-
-    @property
-    def should_poll(self):
-        """No polling needed."""
-        return False
-
-    @property
-    def name(self):
-        """Return the name of the remote."""
-        data = self._data.get(self._hue_id)
-        if data:
-            return data["name"]
-
-    @property
-    def unique_id(self):
-        """Return the ID of this Hue remote."""
-        return self._hue_id
 
     @property
     def state(self):
         """Return the state of the remote."""
-        data = self._data.get(self._hue_id)
-        return data["state"]
+        return self.sensor_data["state"]
 
     @property
     def icon(self):
         """Icon to use in the frontend, if any."""
-        data = self._data.get(self._hue_id)
+        data = self.sensor_data
         if data:
-            icon = ICONS.get(data["model"])
+            icon = REMOTE_ICONS.get(data["model"])
             if icon:
                 return icon
-        return self.ICON
-
-    @property
-    def device_state_attributes(self):
-        """Attributes."""
-        data = self._data.get(self._hue_id)
-        if data:
-            return {key: data.get(key) for key in ATTRS.get(data["model"], [])}
+        _LOGGER.warning(f"No icon found for {self.entity_id}")
+        return "mdi:remote"
 
     @property
     def force_update(self):
